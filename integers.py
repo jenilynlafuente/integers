5e8fd577db8489d947d16d5fd71ea87918b3e8ae
Jun Saldivar <junsaldivar@Juns-MacBook-Air-2.local>
2026-04-29 02:25:04 +0800

    double_triple_integers

--- integers.py
+++ integers.py
@@ -1,6 +1,6 @@
 def process_integers():
     try:
-        with open("integers_txt", "r") as file:
+        with open("integers.txt", "r") as file:
             content = file.read().split()
 
         numbers = [int(num) for num in content]
@@ -13,3 +13,21 @@ def process_integers():
                 even_squares.append(num ** 2)
             else:
                 odd_cubes.append(num ** 3)
+
+        with open("double.txt", "w") as file:
+            for value in even_squares:
+                file.write(str(value) + "\n")
+
+
+        with open("triple.txt", "w") as file:
+            for value in odd_cubes:
+                file.write(str(value) + "\n")
+
+        print("Files created successfully: double.txt and triple.txt")
+
+    except FileNotFoundError:
+        print("Error: integers.txt not found.")
+    except ValueError:
+        print("Error: Make sure integers.txt contains only valid integers.")
+
+process_integers()
